e17d1bbabefa9a23cf9bbfc707fc5d882b4f570a
Igindo\FrankPepermans <frank@igindo.com>
2018-12-22 23:21:03 +0100

    no message

--- lib/src/world/motion/easing.dart
+++ lib/src/world/motion/easing.dart
@@ -12,13 +12,6 @@ Point easeOut(int timeMs, final Vector change, final int durationMs) {
   return Point(change.x * offset, change.y * offset);
 }
 
-Point easeIn(int timeMs, final Vector change, final int durationMs) {
-  final normalizedTime = timeMs > durationMs ? durationMs : timeMs;
-  final offset = math.cos(normalizedTime / durationMs * pi_2);
-
-  return Point(-change.x * offset + change.x, -change.y * offset + change.y);
-}
-
 Point linear(int timeMs, final Vector change, final int durationMs) {
   final normalizedTime = timeMs > durationMs ? durationMs : timeMs;
   final offset = normalizedTime / durationMs;
